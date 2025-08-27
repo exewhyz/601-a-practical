9ac3007539dbf7acff8dbb478d7d619db20b5ce7
exewhyz <araj52444@gmail.com>
2025-08-27 10:15:54 +0530

    dom manipulation and event handelling

---
+++ theory/dom/script.js
@@ -0,0 +1,28 @@
+// document.getElementById("id_name") //string
+// document.getElementsByTagName("tag_name") //[]
+// document.getElementsByClassName("className") //[]
+
+// document.querySelector("#id_name") //string
+// document.querySelector(".className") //string
+// document.querySelector("tag_name") //string
+
+// document.querySelectorAll("") //[]
+
+const element = document.getElementById("first"); //div
+// element.innerHTML = "<button>Click me</button>";
+element.style.backgroundColor = "red";
+element.classList.add("new1");
+element.classList.add("new2");
+element.classList.remove("new1");
+element.classList.toggle("test");
+
+const h3 = document.createElement("h3");
+h3.innerText = "this is heading 3";
+
+document.body.appendChild(h3);
+
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", (event) => {
+  button.textContent = "clicked";
+});
---
+++ theory/js/index.js
@@ -0,0 +1,40 @@
+//object
+// const obj = {
+//   name: "Aniket",
+//   age: 5,
+//   address: {
+//     city: "Chandigarh",
+//     country: "India",
+//   },
+// };
+// console.log(obj.address.city)
+
+// obj.address.pincode = 123456;
+
+// delete obj.age;
+
+// console.log(obj)
+
+//array
+// const arr = [1, 2, 3, "four"];
+
+// console.log(arr[0]) //read
+// arr[2] = "three" //update
+// arr[10] = false //add
+
+// function hello(name,cb) {
+//   console.log("hello", name);
+//   cb();
+// }
+
+// hello("Aniket", ()=>{});
+
+// const test = () => {
+//   console.log("test");
+// };
+
+// test();
+
+
+//DOM
+
