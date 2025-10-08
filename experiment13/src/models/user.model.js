bf42133915b0294d259ac74b2020778d33e502be
exewhyz <araj52444@gmail.com>
2025-10-08 11:14:36 +0530

    Added auth

---
+++ experiment13/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model("User", userSchema);
+
+export default user;
